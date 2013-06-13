4bcf9eeb2475da5aa90b9d33f9dbe0b15dcffa7e
Alyssa Frazee <acfrazee@gmail.com>
2013-06-13 11:32:35 -0400

    fixed checkBid bug

--- 500.R
+++ 500.R
@@ -128,21 +128,40 @@ compareBids = function(bid1, bid2){
 checkBid = function(bid, highBid){
   
   # if you bid something weird:
+  ##[too long or too short]
   if(length(bid)!=2){
-    if(bid!="pass"){
-      theBid = strsplit(readline("invalid bid - try another bid: "), split=" ")[[1]]
-      if(theBid[1]!="pass"){
+    if(bid[1] == "pass") return(bid)
+    if(length(bid)>2 | (length(bid)==1 & bid[1]!="pass")){
+      theBid = strsplit(readline("invalid bid - try again: "), split=" ")[[1]]
+      if(length(theBid)>1){
         if(theBid[2]=="spades") theBid[2] = "aspades"
       }
-      return(checkBid(theBid, highBid))
+      return(checkBid(theBid, highBid))      
+    }    
+  }
+  
+  ##[not bidding a number]
+  if(suppressWarnings(is.na(as.numeric(bid[1])))){
+    theBid = strsplit(readline("invalid bid (bid <NUMBER><space><SUIT>) - try again: "), split=" ")[[1]]
+    if(length(theBid)>1){
+      if(theBid[2]=="spades") theBid[2] = "aspades"
     }
-    if(bid == "pass") return(bid)
+    return(checkBid(theBid, highBid))    
+  }
+  
+  ##[bidding an invalid suit]
+  if(bid[2]!="hearts" & bid[2]!="spades" & bid[2]!="diamonds" & bid[2]!="clubs" & bid[2]!="notrump" & bid[2]!="aspades"){
+    theBid = strsplit(readline("invalid bid (suits are hearts, spades, diamonds, clubs, and notrump) - try again: "), split=" ")[[1]]
+    if(length(theBid)>1){
+      if(theBid[2]=="spades") theBid[2] = "aspades"
+    }
+    return(checkBid(theBid, highBid))
   }
   
   # if you bid less than 6:
-  if(as.numeric(bid[1]) < 6){
+  if(bid[1] < 6){
     theBid = strsplit(readline("please bid at least 6: "), split=" ")[[1]]
-    if(theBid[1]!="pass"){
+    if(length(theBid)>1){
       if(theBid[2]=="spades") theBid[2] = "aspades"
     }
     return(checkBid(theBid, highBid))
@@ -153,7 +172,7 @@ checkBid = function(bid, highBid){
     if(highBid[2]=="aspades") highBid[2] = "spades"
     message(paste0("You must bid higher than the current high bid (",paste(highBid, collapse=" "),")"))
     theBid = strsplit(readline("new bid: "), split=" ")[[1]]
-    if(theBid[1]!="pass"){
+    if(length(theBid)>1){
       if(theBid[2]=="spades") theBid[2] = "aspades"
     }
     return(checkBid(theBid, highBid))
@@ -224,7 +243,7 @@ play500 = function(){
       showHand(hands[[i]])
       if(i==4 | i==3) message(paste0("your partner has bid ", paste(firstTwo[[i-2]], collapse=" ")))
       theBid = strsplit(readline("Please make your bid: "), split=" ")[[1]]
-      if(theBid[1]!="pass"){
+      if(length(theBid)==2){
         if(theBid[2]=="spades") theBid[2] = "aspades"
       }
       theBid = checkBid(theBid, highBid)  # this will ALWAYS result in either a "pass" or a new high bid.
