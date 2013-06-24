6964049c195486c1fa299b108267c42643aff4ed
Allison Kaptur <allison.kaptur@gmail.com>
2013-06-24 11:35:34 -0400

    new test

--- tests.py
+++ tests.py
@@ -4,5 +4,11 @@ def test_deck_creation():
     deck = f.build_deck()
     assert len(deck) == 45
 
+def test_score_table():
+    score_table = f.score_table()
+    assert score_table[f.Bid(10, 'diamonds')] == 480
+    assert score_table[f.Bid(8, 'notrump')] == 320
+
 if __name__ == '__main__':
     test_deck_creation()
+    test_score_table()
